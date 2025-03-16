3b683e197fa0898972bdbb3407fb875b773b094d
mahmouuddd <ben.nacef.mahmoud@gmail.com>
2025-03-16 13:54:43 +0100

    correction

--- cypress/e2e/stepDefs/annonce.js
+++ cypress/e2e/stepDefs/annonce.js
@@ -13,7 +13,7 @@ Given(`I navigate to the annonces page`, () => {
 });
 
 Given(`I see the list of annonces`, () => {
-    cy.get(annonceElem.annonce1).should('be.visible').should('contain','Sousse')
+    cy.get(annonceElem.annonce1).should('be.visible').should('contain','Manouba')
 });
 
 Then(`I should see the details of the annonce`, () => {
